e779d5a70f348bcfd895dcd1fb4b2cd8b33defce
WTrygar <chandelier.wt@gmail.com>
2025-07-17 15:34:42 +0200

    boilerplates and basic TextArea component styling

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex flex-col content-center sm:p-[5%] bg-[url(/src/assets/images/bg-dark-theme.png)] h-100"
+  >
+    <h2 class="text-white text-center text-3xl font-bold">Analyze your text in real-time.</h2>
+    <TextArea :message="message" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import TextArea from './components/TextArea.vue'
+
+const message = ref('')
+console.log(message)
+const charCount = computed(() => {
+  return message.split('').length
+})
+</script>
---
+++ src/components/TextArea.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  message: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <p>{{}}</p>
+  <textarea
+    ref="message"
+    placeholder="Start typing here... (or paste your text)"
+    class="bg-gray-800 text-white resize-y rounded-md"
+  ></textarea>
+  <div class="flex flex-row justify-between text-white">
+    <div>
+      <input type="checkbox" id="one" value="One" />
+      <label for="one">One</label>
+
+      <input type="checkbox" id="two" value="Two" />
+      <label for="two">Two</label>
+    </div>
+    <p>Approx. reading time [value here]</p>
+  </div>
+</template>
