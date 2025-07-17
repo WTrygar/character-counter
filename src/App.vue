b84241e65f1445e7b044039e061a090e21e49dea
WTrygar <chandelier.wt@gmail.com>
2025-07-17 16:04:22 +0200

    feat: CounterBox basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
-<template>
-  <div
-    class="flex flex-col content-center sm:p-[5%] bg-[url(/src/assets/images/bg-dark-theme.png)] h-100"
-  >
-    <h2 class="text-white text-center text-3xl font-bold">Analyze your text in real-time.</h2>
-    <TextArea :message="message" />
-    <p>{{ message }}</p>
-  </div>
-</template>
-
 <script setup>
 import { computed, ref } from 'vue'
+import CounterBox from './components/CounterBox.vue'
 import TextArea from './components/TextArea.vue'
 
 const message = ref('')
@@ -18,3 +9,20 @@ const charCount = computed(() => {
   return message.split('').length
 })
 </script>
+
+<template>
+  <div
+    class="flex flex-col content-center p-[1.5%] bg-[url(/src/assets/images/bg-dark-theme.png)] h-screen"
+  >
+    <!-- <Navbar /> -->
+    <h2 class="text-white text-center text-3xl font-bold">Analyze your text in real-time.</h2>
+
+    <TextArea :message="message" />
+    <div class="flex flex-col gap-2">
+      <CounterBox class="bg-fuchsia-300" />
+      <CounterBox class="bg-orange-400" />
+      <CounterBox class="bg-red-300" />
+    </div>
+    <p>{{ message }}</p>
+  </div>
+</template>
--- src/components/CounterBox.vue
+++ src/components/CounterBox.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-
+  <div class="rounded-lg">
+    <div class="p-2">
+      <p class="text-lg font-bold">[NUMBER]</p>
+      <P>[DESCRIPTION]</P>
+    </div>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
