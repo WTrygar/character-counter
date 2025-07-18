0b4196af0dc13bf08e37365b7d7f45d607a3055f
WTrygar <chandelier.wt@gmail.com>
2025-07-18 15:43:37 +0200

    feat: working toggle between dark and light mode

--- src/components/LetterDensity.vue
+++ src/components/LetterDensity.vue
@@ -6,7 +6,7 @@
         <h4>E</h4>
       </div>
       <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 16.06%"></div>
+        <div class="h-4 rounded-full bg-fuchsia-300" style="width: 16.06%"></div>
       </div>
       <div class="w-4/20 sm:w-3/20 text-right">
         <p>40 (16.06%)</p>
@@ -17,7 +17,7 @@
         <h4>I</h4>
       </div>
       <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 11.65%"></div>
+        <div class="h-4 rounded-full bg-fuchsia-300" style="width: 11.65%"></div>
       </div>
       <div class="w-4/20 sm:w-3/20 text-right">
         <p>29 (11.65%)</p>
@@ -28,7 +28,7 @@
         <h4>T</h4>
       </div>
       <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 11.24%"></div>
+        <div class="h-4 rounded-full bg-fuchsia-300" style="width: 11.24%"></div>
       </div>
       <div class="w-4/20 sm:w-3/20 text-right">
         <p>28 (11.24)</p>
@@ -39,7 +39,7 @@
         <h4>O</h4>
       </div>
       <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 8.84%"></div>
+        <div class="h-4 rounded-full bg-fuchsia-300" style="width: 8.84%"></div>
       </div>
       <div class="w-4/20 sm:w-3/20 text-right">
         <p>22 (8.84%)</p>
@@ -50,40 +50,7 @@
         <h4>N</h4>
       </div>
       <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 8.43%"></div>
-      </div>
-      <div class="w-4/20 sm:w-3/20 text-right">
-        <p>21 (8.43%)</p>
-      </div>
-    </div>
-    <div class="flex flex-row justify-between items-center mt-4">
-      <div class="w-1/20">
-        <h4>N</h4>
-      </div>
-      <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 8.43%"></div>
-      </div>
-      <div class="w-4/20 sm:w-3/20 text-right">
-        <p>21 (8.43%)</p>
-      </div>
-    </div>
-    <div class="flex flex-row justify-between items-center mt-4">
-      <div class="w-1/20">
-        <h4>N</h4>
-      </div>
-      <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 8.43%"></div>
-      </div>
-      <div class="w-4/20 sm:w-3/20 text-right">
-        <p>21 (8.43%)</p>
-      </div>
-    </div>
-    <div class="flex flex-row justify-between items-center mt-4">
-      <div class="w-1/20">
-        <h4>N</h4>
-      </div>
-      <div class="w-7/10 sm:w-8/10 h-4 bg-gray-200 rounded-full dark:bg-gray-800">
-        <div class="h-4 bg-blue-600 rounded-full dark:bg-fuchsia-300" style="width: 8.43%"></div>
+        <div class="h-4 rounded-full bg-fuchsia-300" style="width: 8.43%"></div>
       </div>
       <div class="w-4/20 sm:w-3/20 text-right">
         <p>21 (8.43%)</p>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="flex flex-row justify-between py-2.5">
-    <div>
-      <img src="/src/assets/images/logo-light-theme.svg" alt="Logo" />
-    </div>
-    <a class="bg-gray-200 w-8 h-8 flex p-2 rounded-lg content-center">
-      <img src="/src/assets/images/icon-moon.svg" alt="" />
+    <div
+      class="h-10 w-70 bg-no-repeat bg-[url(/src/assets/images/logo-light-theme.svg)] dark:bg-[url(/src/assets/images/logo-dark-theme.svg)]"
+    ></div>
+    <a
+      @click="toggleDark()"
+      class="bg-gray-200 dark:bg-gray-800 w-9 h-9 flex p-1.5 rounded-lg content-center"
+    >
+      <div
+        class="h-7.5 w-7.5 bg-[url(/src/assets/images/icon-moon.svg)] dark:bg-[url(/src/assets/images/icon-sun.svg)] bg-no-repeat"
+      ></div>
     </a>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
